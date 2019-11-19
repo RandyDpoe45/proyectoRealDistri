2a5a6807c5e3991d52ddeb20152ae8ab99b08a66
gamendez98 <gamendez98@gmail.com>
2019-11-18 23:15:46 -1000

    gui for ofertas started falta un popup de candidatos

--- Empresa/src/presentacion/EmpresaGUI.java
+++ Empresa/src/presentacion/EmpresaGUI.java
@@ -7,7 +7,6 @@ package presentacion;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -24,63 +23,60 @@ import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 import negocio.Candidato;
-import negocio.ExperienciaLaboral;
 import negocio.Oferta;
 import negocio.SectorEmpresa;
 import negocio.Sobre;
 import persistencia.Reader;
-import servidor.CandidatoCliente;
-import servidor.ImplementacionCandidatoCliente;
-import servidor.OperacionesCandidato;
+import servidor.Empresa;
+import servidor.ImplementacionOfertaCliente;
+import servidor.OfertaCliente;
+import servidor.OperacionesOferta;
 
 /**
  *
  * @author gustavo
  */
-public class ClienteGUI extends javax.swing.JFrame {
+public class EmpresaGUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form Cliente
+     * Creates new form Empresa
      */
     private static String ServerIP="127.0.0.1";
     private static String IP="127.0.0.1";
     private static int port;
-    private static OperacionesCandidato stub;
-    private static List<Candidato> candidatos;
-    public ClienteGUI() {
+    private static OperacionesOferta stub;
+    private static List<Oferta> ofertas;
+    public EmpresaGUI() {
         initComponents();
     }
     
     private void conectar(){
-        ImplementacionCandidatoCliente.gui=this;
         try {
             IP = java.net.InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException ex) { 
-            Logger.getLogger(ClienteGUI.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(Empresa.class.getName()).log(Level.SEVERE, null, ex);
         }
         System.out.println(IP);
         try {
-            candidatos=new ArrayList<>();
+            ofertas=new ArrayList<>();
             //System.setProperty("java.rmi.server.hostname","192.168.43.171");
-            ImplementacionCandidatoCliente icc=new ImplementacionCandidatoCliente(candidatos);
-            CandidatoCliente cc= (CandidatoCliente) UnicastRemoteObject.exportObject(icc, port);
+            ImplementacionOfertaCliente ioc=new ImplementacionOfertaCliente(ofertas);
+            OfertaCliente oc=(OfertaCliente) UnicastRemoteObject.exportObject(ioc, port);
             Registry reg = LocateRegistry.createRegistry(port);
-            reg.rebind("CandidatoCliente", cc);
+            reg.rebind("OfertaCliente", oc);
             Registry registry = LocateRegistry.getRegistry(ServerIP, 9635);
-            stub = (OperacionesCandidato) registry.lookup("Candidato");
+            stub = (OperacionesOferta) registry.lookup("Oferta");
             stub.registrar(IP, port);
-            
+            //System.out.println(response.getCargo());
         } catch (RemoteException ex) {
-            Logger.getLogger(ClienteGUI.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Oferta.class.getName()).log(Level.SEVERE, null, ex);
         } catch (NotBoundException ex) {
-            Logger.getLogger(ClienteGUI.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Oferta.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    
-    public void actualizarEntradas() throws InterruptedException{
+        public void actualizarEntradas() throws InterruptedException{
         DefaultTableModel dm;
         dm=(DefaultTableModel) Entradas.getModel();
         int rowCount = dm.getRowCount();
@@ -88,14 +84,14 @@ public class ClienteGUI extends javax.swing.JFrame {
         for (int i =0; i <rowCount; i++) {
             dm.removeRow(0);
         }
-        for(Candidato c:candidatos){
-            Oferta of=c.getOfertaAsignadas();
+        for(Oferta o:ofertas){
             Object[] row=new Object[]{
-                c.getNombre(), 
-                c.getDocumento(), 
-                c.getNivelEstudios(),
-                c.getAspiracionLaboral(),
-                (of!=null)?of.getId():"none"
+                o.getIdentificador(),
+                o.getCargo(),
+                o.getNivelEstudios(),
+                o.getExperienciaRequerida(),
+                o.getSalarioOfrecido(),
+                o.getSectorEmpresa()
             };
             dm.addRow(row);
         }
@@ -106,10 +102,11 @@ public class ClienteGUI extends javax.swing.JFrame {
     }
     
     private static Map<String,SectorEmpresa> stringToSector=new HashMap<String,SectorEmpresa>();
-    public void read(String file,OperacionesCandidato stub,String IP,int port,List<Candidato> listaCandidatos){ 
+    private void read(String file,OperacionesOferta stub,String IP,int port,List<Oferta> listaOfertas){
         stringToSector.put("comercio", SectorEmpresa.comercio);
         stringToSector.put("financiero", SectorEmpresa.financiero);
         stringToSector.put("manufactura", SectorEmpresa.manufactura);
+        listaOfertas = new ArrayList<>();
 
         File archivoCandidatos = new File(file);
 
@@ -121,46 +118,31 @@ public class ClienteGUI extends javax.swing.JFrame {
         while ((st = br.readLine()) != null) {
             String[] info_candidato = st.split(" ");
 
-            String nombre = info_candidato[0];
-            String documento = info_candidato[1];
-            int nivel_estudios = Integer.parseInt(info_candidato[2]);
-            float aspiracion_salarial = Float.parseFloat(info_candidato[3].trim());
-
-            Candidato candidato = new Candidato(nombre, documento, nivel_estudios, aspiracion_salarial);
-            List<ExperienciaLaboral> listaExperienciasLaborales = new ArrayList<>();
-
-            while (!"0".equals(st = br.readLine())) {
-                String[] experiencia_laboral = st.split(" ");
-
-                String cargo = experiencia_laboral[0];
-                int duracion = Integer.parseInt(experiencia_laboral[1].trim());
-                SectorEmpresa sector = stringToSector.get(experiencia_laboral[2]);
-
-                ExperienciaLaboral info_experiencia_laboral = new ExperienciaLaboral(cargo, duracion, sector);
-                listaExperienciasLaborales.add(info_experiencia_laboral);
-            }
-            candidato.setExperiencia(listaExperienciasLaborales);
+            String cargo = info_candidato[0];
+            int nivel_estudios = Integer.parseInt(info_candidato[1]);
+            int experiencia = Integer.parseInt(info_candidato[2]);
+            float salario = Float.parseFloat(info_candidato[3].trim());
+            SectorEmpresa sector=stringToSector.get(info_candidato[4].trim());
+            
+            Oferta oferta = new Oferta(cargo, nivel_estudios,experiencia, salario,sector);
 
             try {
-                Candidato output=stub.registrarCandidato(new Sobre<Candidato>(IP+":"+port,candidato),new ArrayList());
-                System.out.println(output);
-                candidato=output;
-                
+                Oferta resultado=stub.registrarOferta(new Sobre<Oferta>(IP+":"+port,oferta),new ArrayList<>());
+                System.out.println(resultado);
+                oferta=resultado;
             } catch (RemoteException e) {
                 System.out.println("Excepcion: " + e);
             }
 
-            listaCandidatos.add(candidato);
+            ofertas.add(oferta);
             actualizarEntradas();
             int tiempo_espera = Integer.parseInt(br.readLine().trim());
             Thread.sleep(tiempo_espera * 1000);
         }
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(servidor.Cliente.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(servidor.Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Empresa.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
-            Logger.getLogger(servidor.Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Empresa.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
@@ -173,26 +155,22 @@ public class ClienteGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        IPLabel = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         IPTextField = new javax.swing.JTextField();
         portTextField = new javax.swing.JTextField();
         startButton = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        Scroll = new javax.swing.JScrollPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
         Entradas = new javax.swing.JTable();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.Y_AXIS));
 
-        IPLabel.setText("ingresar IP del server y port local");
-        getContentPane().add(IPLabel);
+        jLabel1.setText("ingresar IP del server");
+        getContentPane().add(jLabel1);
 
         IPTextField.setText("127.0.0.1");
-        IPTextField.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                IPTextFieldActionPerformed(evt);
-            }
-        });
         getContentPane().add(IPTextField);
 
         portTextField.setText("15000");
@@ -213,20 +191,22 @@ public class ClienteGUI extends javax.swing.JFrame {
 
             },
             new String [] {
-                "Nombre", "Documento", "Estudios", "Aspiracion", "Oferta"
+                "oferta", "cargo", "estudios", "experiencia", "salario", "sector"
             }
         ));
-        Scroll.setViewportView(Entradas);
+        jScrollPane1.setViewportView(Entradas);
 
-        jPanel1.add(Scroll);
+        jPanel1.add(jScrollPane1);
 
         getContentPane().add(jPanel1);
+
+        jButton2.setText("ver candidatos");
+        getContentPane().add(jButton2);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void startButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_startButtonMouseClicked
-        // TODO add your handling code here:
         System.out.println("started...");
         ServerIP=IPTextField.getText().trim();
         port=Integer.decode(portTextField.getText().trim());
@@ -234,15 +214,11 @@ public class ClienteGUI extends javax.swing.JFrame {
         Thread t=new Thread(){
             @Override
             public void run(){
-                read("./src/persistencia/candidatos.txt", stub, IP,port,candidatos);
+                read("./src/persistencia/ofertas.txt", stub, IP,port,ofertas);
             }
         };
         t.start();
     }//GEN-LAST:event_startButtonMouseClicked
-
-    private void IPTextFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_IPTextFieldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_IPTextFieldActionPerformed
 
     /**
      * @param args the command line arguments
@@ -261,13 +237,13 @@ public class ClienteGUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ClienteGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EmpresaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ClienteGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EmpresaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ClienteGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EmpresaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ClienteGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EmpresaGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
         //</editor-fold>
@@ -275,17 +251,18 @@ public class ClienteGUI extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ClienteGUI().setVisible(true);
+                new EmpresaGUI().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTable Entradas;
-    private javax.swing.JLabel IPLabel;
     private javax.swing.JTextField IPTextField;
-    private javax.swing.JScrollPane Scroll;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField portTextField;
     private javax.swing.JButton startButton;
     // End of variables declaration//GEN-END:variables
